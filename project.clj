43d917d41d24b81cb43c5c8f3f5dff503e988f1d
Sean Toner <placeoftheway@gmail.com>
2015-12-02 10:48:42 -0500

    Adding repl init option to start with other namespace

--- project.clj
+++ project.clj
@@ -6,4 +6,7 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [clj-ssh "0.5.11"]
                  [com.taoensso/timbre "4.1.4"]
-                 [org.clojure/core.async "0.2.371"]])
+                 [org.clojure/core.async "0.2.371"]]
+
+  :repl-options {:init-ns commando.command}
+  )
